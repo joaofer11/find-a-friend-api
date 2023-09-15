395f49fbac96b667ecbc35ec80e3718b69c90993
João Fernandes <joaofergear003@gmail.com>
2023-09-15 14:41:51 -0300

    feat: add pet enums

---
+++ src/custom-types/pet-enums.ts
@@ -0,0 +1,9 @@
+type TAge = 'NEW_BORN' | 'JUNIOR' | 'TEEN' | 'FULL_GROW';
+
+type TEnvironment = 'CLOSED' | 'OPEN' | 'WIDE_OPEN';
+
+type TLevel = 'LOW' | 'MEDIUM' | 'HIGH' | 'VERY_HIGH';
+
+type TSize = 'VERY_SMALL' | 'SMALL' | 'MEDIUM' | 'BIG' | 'VERY_BIG';
+
+export { TAge, TEnvironment, TLevel, TSize };
