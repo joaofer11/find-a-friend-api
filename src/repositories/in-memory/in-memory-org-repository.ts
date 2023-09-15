8c37883a45f1bb8d064227c4a9c9fd02f53f54f9
João Fernandes <joaofergear003@gmail.com>
2023-09-15 14:34:57 -0300

    repository: implement org interface into memory

---
+++ src/repositories/in-memory/in-memory-org-repository.ts
@@ -0,0 +1,30 @@
+import { randomUUID } from 'node:crypto';
+import { Org, Prisma } from '@prisma/client';
+import { IOrgRepository } from '../interface-org-repository';
+
+export class InMemoryOrgRepository implements IOrgRepository {
+  items: Org[] = [];
+
+  async add(data: Prisma.OrgCreateInput) {
+    const org = {
+      id: data.id ?? randomUUID(),
+      name: data.name,
+      description: data.description ?? null,
+      email: data.email,
+      passwordHash: data.passwordHash,
+      state: data.state,
+      city: data.city,
+      address: data.address,
+      postalCode: data.postalCode,
+      whatsAppNumber: data.whatsAppNumber,
+    };
+
+    this.items.push(org);
+
+    return org;
+  }
+
+  async findById(id: string) {
+    return this.items.find((item) => item.id === id) ?? null;
+  }
+}
