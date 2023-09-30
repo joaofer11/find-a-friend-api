b45ca121cefd90f04f2919b124ea07b639cbf88b
João Fernandes <joaofergear003@gmail.com>
2023-09-30 15:45:37 -0300

    fix: add missing script to log prisma query for dev environment

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,5 +1,6 @@
+import { env } from '@/env';
 import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient({
-  log: env,
+  log: env.NODE_ENV === 'dev' ? ['query'] : [],
 });
